55155b88344e18574d58e259ba2d74abd5d24772
samuelcaldas <samuel.caldas@gmail.com>
2026-08-29 11:40:45 -0300

    docs: sync NinjaTraderAddOnProject sample configuration

--- samples/NinjaTraderAddOnProject/NinjaTraderAddOnProject/Properties/AssemblyInfo.cs
+++ samples/NinjaTraderAddOnProject/NinjaTraderAddOnProject/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NinjaTraderAddOnProject")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Reference AddOn")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NinjaTraderAddOnProject")]
